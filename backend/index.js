338a0f9b095a3b45ba65c24592d0a804ac334c96
Jyoti Sharma <83448327+JYOTICREATION@users.noreply.github.com>
2021-12-09 17:59:40 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,44 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mysql = require("mysql");
-const port = 7000;
-const app = express();
-app.use(bodyParser.json());
+var express = require("express");
+var app = express();
+var fs = require("fs");
 
-const db = mysql.createPool({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "movie",
-});
-app.get("/", (req, res) => {
-  res.send("Home Page");
+app.get("/getUsers", function (req, res) {
+  fs.readFile(__dirname + "/" + "movielist.json", "utf8", function (err, data) {
+    console.log(data);
+    res.end(data);
+  });
 });
 
-app.listen(port, () => {
-  console.log("listening at http://localhost:${port}");
+var movie = {
+  movie5: {
+    id: 5,
+    name: "Sholay",
+    rating: 3,
+    rel_date: "4 - Oct - 1999",
+  },
+};
+
+app.post("/addMovie", function (req, res) {
+  fs.readFile(__dirname + "/" + "movielist.json", "utf8", function (err, data) {
+    data = JSON.parse(data);
+
+    data["movie5"] = movie["movie5"];
+    console.log(data);
+    res.end(JSON.stringify(data));
+  });
+});
+
+app.get("/:id", function (req, res) {
+  fs.readFile(__dirname + "/" + "movielist.json", "utf8", function (err, data) {
+    var movies = JSON.parse(data);
+    var movie = movies["user" + req.params.id];
+    console.log(movie);
+    res.end(JSON.stringify(movie));
+  });
+});
+
+var server = app.listen(7000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("listening at http://%s:%s", host, port);
 });
